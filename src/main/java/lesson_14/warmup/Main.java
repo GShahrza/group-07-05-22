d2cd1eea635fec4dc168b459308da1e3badab9fa
Shahrza Gahramanov <gshahrza@gmail.com>
2022-06-05 20:41:29 +0400

    lesson14

---
+++ src/main/java/lesson_14/warmup/Main.java
@@ -0,0 +1,13 @@
+package main.java.lesson_14.warmup;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(User.info);
+
+        User user1 = new User("Shahrza","Gahramanov",1);
+
+        User user2 = new User("X","X",2);
+
+    }
+}
